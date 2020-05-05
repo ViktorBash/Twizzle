00fb994ed4bac94c916663ba0fa73050f29832fa
Viktor Basharkevich <basharkevichv@gmail.com>
2020-05-05 14:15:45 -0400

    Now when logged in, User can see the their boards on the home screen, and nobody elses

--- twizzle/todoboard/urls.py
+++ twizzle/todoboard/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
-from .views import home
+from .views import home, HomeView
 
 urlpatterns = [
-    path('', home, name='todoboard-home')
+    path('', HomeView.as_view(), name='todoboard-home'),
 ]
--- twizzle/todoboard/views.py
+++ twizzle/todoboard/views.py
@@ -1,6 +1,43 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Board, Items
+from django.views.generic import (ListView,
+                                  DetailView,
+                                  CreateView,
+                                  UpdateView,
+                                  DeleteView,
+                                  )
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def home(request):
     return render(request, 'todoboard/home.html')
+
+
+class HomeView(ListView, UserPassesTestMixin):
+    model = Board
+    template_name = "todoboard/home.html"
+    context_object_name = "boards"
+    ordering = ['-date_posted']
+
+    # def test_func(self):
+    #     board = self.get_object()
+    #     if self.request.user == board.author:
+    #         return True
+    #     return False
+
+    def get_queryset(self):
+        return Board.objects.all().filter(author=self.request.user)
+
+    # def get_context_data(self, **kwargs):
+    #     user = get_object_or_404(User, username=self.kwargs.get('username'))
+    #
+    #     context = super(HomeView, self).get_context_data(**kwargs)
+    #     context['board'] = Board.objects.filter(author=user).order_by
+    #     return context
+
+    # def get_queryset(self):
+    #     user = get_object_or_404(User, username=self.kwargs.get('username'))
+    #     return Board.objects.filter(author=user).order_by('-date_posted')
+
