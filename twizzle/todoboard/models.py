dd8c35a3492399cab02859a85c0fb1e89ec9beec
Viktor Basharkevich <basharkevichv@gmail.com>
2020-05-05 00:55:16 -0400

    Added 2 models and worked on front end

--- twizzle/todoboard/models.py
+++ twizzle/todoboard/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.utils import timezone
 # Create your models here.
+
+
+class Board(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_posted = models.DateTimeField(default=timezone.now)
+
+
+class Items(models.Model):
+    board = models.ForeignKey(Board, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    content = models.TextField(default="")
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
